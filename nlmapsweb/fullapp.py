f27f8ba564d4f7d8ae9c3d0db6da914af021af4d
Simon Will <will@cl.uni-heidelberg.de>
2021-01-16 14:05:53 +0100

    Add config for logfile

--- nlmapsweb/fullapp.py
+++ nlmapsweb/fullapp.py
@@ -1,4 +1,5 @@
 import logging.config
+from pathlib import Path
 import sys
 
 from nlmapsweb.app import create_app
@@ -19,11 +20,16 @@ logging.config.dictConfig({
             'class': 'logging.StreamHandler',
             'stream': sys.stdout,
             'formatter': 'default'
+        },
+        'logfile': {
+            'class': 'logging.FileHandler',
+            'filename': str((Path.home() / 'nlmapsweb.log').resolve()),
+            'formatter': 'default'
         }
     },
     'root': {
         'level': 'INFO',
-        'handlers': []
+        'handlers': ['logfile']
     }
 })
 
