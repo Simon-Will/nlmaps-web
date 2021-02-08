012e1526a1b273acd280cbfd7a5e183aee4f0593
Simon Will <will@cl.uni-heidelberg.de>
2021-02-08 11:42:40 +0100

    Change development model

--- nlmapsweb/config/development.py
+++ nlmapsweb/config/development.py
@@ -2,8 +2,6 @@ import logging
 import sys
 
 MODELS = [
-    'will_nlmaps_3delta.noise.plusv2_lin_char_legacy.yaml',
-    'will_nlmaps_3gamma.noise.plusv2_online_incorrect_bs10_ratio0.5.yaml'
+    'will_nlmaps_3delta.noise.plusv2_web2to1_ratio05_fixed.yaml',
 ]
-#CURRENT_MODEL = 'will_nlmaps_3gamma.noise.plusv2_online_incorrect_bs10_ratio0.5.yaml'
-CURRENT_MODEL = 'will_nlmaps_3delta.noise.plusv2_lin_char_legacy.yaml'
+CURRENT_MODEL = 'will_nlmaps_3delta.noise.plusv2_web2to1_ratio05_fixed.yaml'
