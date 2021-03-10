68a839c26c8a1b577f9ac9275e80288c69746143
Simon Will <will@cl.uni-heidelberg.de>
2021-03-10 19:59:52 +0100

    Remove old answering code and concentrate on nlmaps-tools answering

--- nlmapsweb/processing/answering.py
+++ nlmapsweb/processing/answering.py
@@ -4,130 +4,65 @@ import traceback
 
 from flask import current_app
 
-from nlmaps_tools.answer_mrl import load_features, answer, merge_feature_collections
+from nlmaps_tools.answer_mrl import (
+    load_features, answer as get_answer, merge_feature_collections
+)
 
 from nlmapsweb.processing.result import Result
+
 
 def answer_query(mrl_query):
     current_app.logger.info('Interpreting query "{}".'.format(mrl_query))
 
     features = load_features(mrl_query)
     if features:
-        result = answer(features)
-        current_app.logger.info('Received py answering result'.format(result))
+        result = get_answer(features)
+        current_app.logger.info('Received py answering result')
         if 'centers' in result:
-            result['geojson'] = merge_feature_collections(result['centers'],
-                                                          result['targets'])
+            result['geojson'] = merge_feature_collections(
+                result.get('centers'), result.get('targets')
+            )
             del result['centers']
-        else:
+        elif 'targets' in result:
             result['geojson'] = result['targets']
-        del result['targets']
+
+        if 'targets' in result:
+            del result['targets']
+
         return result
 
-    answer_cmd = current_app.config['ANSWER_COMMAND']
-    try:
-        proc = subprocess.run(answer_cmd, capture_output=True, input=mrl_query,
-                              text=True, check=True)
-    except:
-        current_app.logger.warning(traceback.format_exc())
-        current_app.logger.warning('Interpreting query "{}" failed.'.format(mrl_query))
-        return False
-
-    result = proc.stdout.strip()
-    current_app.logger.info('Received answering result {}'.format(result))
-    return result
-
-
-def get_geojson_features(mrl_query):
-    current_app.logger.info('Interpreting query "{}".'.format(mrl_query))
-    answer_cmd = current_app.config['ANSWER_COMMAND']
-    try:
-        proc = subprocess.run(answer_cmd, capture_output=True, input=mrl_query,
-                              text=True, check=True)
-    except:
-        current_app.logger.warning(traceback.format_exc())
-        current_app.logger.warning('Interpreting query "{}" failed.'.format(mrl_query))
-        return False
-
-    if proc.stdout.startswith('"features": '):
-        current_app.logger.info('Received result {}'.format(proc.stdout))
-        features = proc.stdout[12:].strip()
-        print(features, file=open('/tmp/geo.json', 'w'))
-        return json.loads(features)
-
-    current_app.logger.info('No features found.')
-    return []
+    return {'error': 'Unknown MRL interpretation error'}
 
 
 class AnswerResult(Result):
 
-    def __init__(self, success, mrl, answer, features, error=None, py_result=None):
+    def __init__(self, success, mrl, result, error=None):
         super().__init__(success, error)
         self.mrl = mrl
-        self.answer = answer
-        self.features = features
-        self.geojson = {'type': 'FeatureCollection', 'features': features}
 
-        if py_result:
-            if 'geojson' in py_result:
-                self.geojson = py_result.pop('geojson')
-                self.features = self.geojson['features']  # Do we need this?
-            else:
-                self.geojson = {'type': 'FeatureCollection', 'features': []}
+        if 'geojson' in result:
+            self.geojson = result.pop('geojson')
+        else:
+            self.geojson = {'type': 'FeatureCollection', 'features': []}
 
-            self.answer = py_result
+        if 'error' in result:
+            current_app.logger.error('MRL interpretation error: {}'
+                                     .format(result['error']))
+
+        self.answer = result
 
     @classmethod
     def from_mrl(cls, mrl):
         result = answer_query(mrl)
 
-        if isinstance(result, dict):
-            if result.get('type') == 'error':
-                success = False
-                error = result['error']
-            else:
-                success = True
-                error = None
-            return cls(success=success, mrl=None, answer=None, features=None,
-                       error=error, py_result=result)
-
-        if result is False:
-            error = f'Failed to parse MRL query {mrl!r}'
-            current_app.logger.warning(error)
-            return cls(success=False, mrl=mrl, answer=None, features=[],
-                       error=error)
-
-        parts = result.strip().split('\n')
-        if len(parts) != 2:
-            error = f'Unexpected answering result: {result!r}'
-            current_app.logger.warning(error)
-            if len(parts) == 1:
-                # XXX: The answer seems left out. For now, we’re trying to
-                # recover by assuming an empty answer.
-                parts.insert(0, 'No answer found.')
-            else:
-                return cls(success=False, mrl=mrl, answer=None, features=[],
-                           error=error)
-
-        answer = parts[0]
-        features_str = parts[1]
-
-        if features_str.startswith('"features": '):
-            features_str = features_str[12:].strip()
-            try:
-                features = json.loads(features_str)
-            except json.JSONDecodeError:
-                error = f'Error decoding the following JSON: {features_str!r}'
-                current_app.logger.warning(error)
-                return cls(success=False, mrl=mrl, answer=answer, features=[],
-                           error=error)
+        if result.get('type') == 'error':
+            success = False
+            error = result['error']
         else:
-            error = f'Could not find features in the string {features_str!r}'
-            current_app.logger.warning(error)
-            return cls(success=False, mrl=mrl, answer=answer, features=[],
-                        error=error)
+            success = True
+            error = None
 
-        return cls(success=True, mrl=mrl, answer=answer, features=features)
+        return cls(success=success, mrl=None, result=result, error=error)
 
     def to_dict(self):
         return {'success': self.success, 'error': self.error,
