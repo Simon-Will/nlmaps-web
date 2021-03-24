cabdac98a6701f1c11646a3c169cbabe6fe3643b
Simon Will <will@cl.uni-heidelberg.de>
2021-03-24 10:57:44 +0100

    Add plot to validations view

--- nlmapsweb/utils/plotting.py
+++ nlmapsweb/utils/plotting.py
@@ -1,4 +1,5 @@
 import base64
+from collections import defaultdict
 import io
 
 import matplotlib
@@ -6,27 +7,24 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
-def plot_tagged_percentages(tag_counts, total, width=0.35):
+def plot_validations(validations):
+    validations_by_label = defaultdict(list)
+    for validation in validations:
+        validations_by_label[validation['label']].append(validation)
+
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    sorted_tags = sorted(tag_counts.keys())
-    percentages_tagged = [tag_counts[tag] / total for tag in sorted_tags]
-    percentages_left = [1 - perc for perc in percentages_tagged]
-    ind = range(len(sorted_tags))
 
-    plt_tagged, *_ = ax.barh(ind, percentages_tagged, width, color='red')
-    ax.barh(ind, percentages_left, width, left=percentages_tagged,
-            color='lightgray')
+    for label, vals in validations_by_label.items():
+        artist = ax.plot(
+            [val['created'] for val in vals],
+            [val['accuracy'] for val in vals],
+            label=label
+        )
 
-    #sorted_tags = [chr(0x61 + i) for i in range(len(sorted_tags))]
-    ax.set_title('Percentage of Tagged Instances by Tag')
-    ax.set_xticks([0.2, 0.4, 0.6, 0.8, 1.0])
-    ax.set_xlabel('Percentage')
-    ax.set_yticks(ind)
-    ax.set_yticklabels(sorted_tags, fontsize=8)
-    fig.subplots_adjust(left=0.30)
-
-    #ax.legend([plt_tagged], ['Percentage Tagged'])
+    ax.legend()
+    ax.set_title('Evalution Results over Time')
+    ax.set_ylim((0.0, 1.0))
 
     return fig
 
--- nlmapsweb/views/validations.py
+++ nlmapsweb/views/validations.py
@@ -1,8 +1,11 @@
+import datetime as dt
+
 from flask_login import current_user
 from flask import current_app, render_template, request
 
 import nlmapsweb.mt_server as mt_server
 from nlmapsweb.utils.auth import admin_required
+from nlmapsweb.utils.plotting import fig_to_base64, plot_validations
 
 
 @current_app.route('/validations', methods=['GET'])
@@ -14,7 +17,13 @@ def validations():
     else:
         params = None
     response = mt_server.get('/validations', params=params)
-    evaluation_results = response.json()
 
-    return render_template('validations.html',
-                           evaluation_results=evaluation_results)
+    eval_results = response.json()
+    for val in eval_results:
+        val['created'] = dt.datetime.fromisoformat(val['created'])
+
+    figure = plot_validations(eval_results)
+    base64_jpg = fig_to_base64(figure, 'jpg')
+
+    return render_template('validations.html', validations=eval_results,
+                           base64_jpg=base64_jpg)
