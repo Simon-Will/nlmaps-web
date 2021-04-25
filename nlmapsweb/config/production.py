04245730fe1389cee10b7ac25bc4e612b9690647
Simon Will <will@cl.uni-heidelberg.de>
2021-04-25 20:30:58 +0200

    Change production model

--- nlmapsweb/config/production.py
+++ nlmapsweb/config/production.py
@@ -18,6 +18,7 @@ MODELS = [
     'n3epsilon.noise.plusv2_web2to1_ratio05_prod.yaml',
     'n3epsilon.noise.plusv2_5515_prod.yaml',
     'n3_ar_ratio05.yaml',
+    'n3_arc_ratio05.yaml',
 ]
-CURRENT_MODEL = 'n3_ar_ratio05.yaml'
+CURRENT_MOrEL = 'n3_arc_ratio05.yaml'
 FALLBACK_MODEL = 'n3.yaml'
